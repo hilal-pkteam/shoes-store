8729d0d4bd605594c88ae1c799f10f949ae21898
Hilal Ahmad <hilal@knowpakistan.org>
2024-05-13 21:30:24 +0500

    Main Layout Created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Charsada Chappal | Online Store</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layout.app')
+@section('title', 'Home | Shoes Store')
+
+@section('content')
+    <h1>Welcome to My Website</h1>
+    <p>This is the home page.</p>
+@endsection
