170191cc356a5d7917f33ca2249ad9a2e06378b9
Hilal Ahmad <hilal@knowpakistan.org>
2024-05-15 21:54:32 +0500

    Cards Section & Footer Done | Home Page

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -2,7 +2,7 @@
 
       <div class="child w-full md:w-3/5 lg:w-2/5 md:my-auto md:mx-12 px-8">
       
-         <h1 class="text-3xl md:text-5xl md:w-full md:font-medium">Sell And Buy Laptops</h1>
+         <h1 class="text-3xl md:text-4xl md:w-full md:font-medium">Buy Shoes At Your Comfort</h1>
          <p class="my-3 text-gray-700 font-medium">Trusted platform for buying and selling laptops. Direct customer-to-customer interactions. Find your perfect laptop now</p>
          <button id="buy-button" class="my-2 bg-white hover:bg-blue-600 hover:text-white text-gray-800 font-semibold py-2 px-4 border border-gray-500 rounded shadow">Buy Now</button>
          <a href="#" onclick="redirectToPost()"><button class="my-2 bg-white hover:bg-slate-800 hover:text-white text-gray-800 font-semibold py-2 px-6 border border-gray-500 rounded shadow">Sell</button></a>
@@ -22,13 +22,5 @@
     });
   });
 
-  // Redirecting to login/post page on some condition
-
-function redirectToPost() 
-{
-   var redirectUrl = <?php echo isset($_SESSION['loggedin']) ? "'/post-create?id=" . $_SESSION['loggedin']['id'] . "'" : "'/user-login'"; ?>;
-
-   window.location.href = redirectUrl;
-}
 
 </script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,85 @@
 @include('layouts.header')
 
 @include('partials.navbar')
+<form action="/search" method="POST" class="flex items-center my-5 mx-7">
+    <div class="relative w-full">
+        <input type="text" id="simple-search" name="search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Quality Laptops" required="">
+    </div>
+    <button type="submit" name="search-button" class="px-6 py-2 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        <span class="sr-only">Search</span>
+    </button>
+  </form>
+
 @include('partials.hero')
 
-<div class="m-16">
-    <h2 class="text-4xl font-medium">Home Page</h2>
-</div>
+  <h1 class="text-2xl text-center sm:text-3xl lg:text-4xl xl:text-5xl font-medium text-zinc-900">Fresh Items</h1>
+  <br><br><br><br>
+
+  <!-- Cards -->
+  <section class="grid grid-cols-1 mx-auto gap-3 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 max-w-7xl sm:mx-5">
+    
+    <div class="max-w-sm mx-auto rounded overflow-hidden shadow-md cursor-pointer border border-gray-300">
+        <img class="w-full h-60 object-cover" src="{{ asset('images/dell_xps.jpg') }}" alt="Sunset in the mountains" loading="lazy">
+        <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">laptop Name</div>
+        <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis asperiores reprehenderit possimus. 
+        </p>
+        <h3 class="text-base font-bold text-gray-600 mt-2 -mb-3">RS 44444</h3>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Formated Date</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Laptop condition</span>
+        </div>
+    </div>
+
+    <div class="max-w-sm mx-auto rounded overflow-hidden shadow-md cursor-pointer border border-gray-300">
+        <img class="w-full h-60 object-cover" src="{{ asset('images/dell_xps.jpg') }}" alt="Sunset in the mountains" loading="lazy">
+        <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">laptop Name</div>
+        <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis asperiores reprehenderit possimus. 
+        </p>
+        <h3 class="text-base font-bold text-gray-600 mt-2 -mb-3">RS 44444</h3>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Formated Date</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Laptop condition</span>
+        </div>
+    </div>
+
+    <div class="max-w-sm mx-auto rounded overflow-hidden shadow-md cursor-pointer border border-gray-300">
+        <img class="w-full h-60 object-cover" src="{{ asset('images/dell_xps.jpg') }}" alt="Sunset in the mountains" loading="lazy">
+        <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">laptop Name</div>
+        <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis asperiores reprehenderit possimus. 
+        </p>
+        <h3 class="text-base font-bold text-gray-600 mt-2 -mb-3">RS 44444</h3>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Formated Date</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Laptop condition</span>
+        </div>
+    </div>
+
+    <div class="max-w-sm mx-auto rounded overflow-hidden shadow-md cursor-pointer border border-gray-300">
+        <img class="w-full h-60 object-cover" src="{{ asset('images/dell_xps.jpg') }}" alt="Sunset in the mountains" loading="lazy">
+        <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">laptop Name</div>
+        <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis asperiores reprehenderit possimus. 
+        </p>
+        <h3 class="text-base font-bold text-gray-600 mt-2 -mb-3">RS 44444</h3>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Formated Date</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Laptop condition</span>
+        </div>
+    </div>
+
+  </section>
+  @include('partials.footer')
 
 @include('layouts.footer')
